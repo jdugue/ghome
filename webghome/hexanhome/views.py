3f0e4567ca01cece09951c11dcda562b121687d0
jdugue <jossdug@gmail.com>
2013-12-24 00:08:48 +0100

    avancement de l'app. mise en place du frontend + login page

--- webghome/hexanhome/views.py
+++ webghome/hexanhome/views.py
@@ -1,10 +1,33 @@
 #-*- coding: utf-8 -*-
 
-from django.shortcuts import render; render_to_response
+from django.views.generic import TemplateView
+
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+from django.template import RequestContext
+
+from django.shortcuts import *
+
 from hexanhome.models import *
 
-# Create your views here.
+def index(request):
+	# template = loader.get_template('hexanhome/index.html')
+	return render(request , 'hexanhome/index.html')
+
+def login(request):
+    return render_to_response('home.html', RequestContext(request))
+
+def logout(request):
+    logout(request)
+    return HttpResponseRedirect('/home')
+
+@login_required
 def home(request):
 	list_capteurs = Capteur.objects.all()
+	list_users = User.objects.all()
 
 	return render_to_response('hexanhome/home.html', { 'list_capteurs': list_capteurs })
--- webghome/webghome/urls.py
+++ webghome/webghome/urls.py
@@ -3,11 +3,17 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+# from hexanhome.views import HomeView
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'webghome.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^home/', include(hexanhome.views.home)),
+    # url(r'^home/$', HomeView.as_view(), name='home'),
+    url(r'^$', 'hexanhome.views.index'),
+    url(r'^home/$', 'hexanhome.views.home'),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', 'logout'),	
 )
