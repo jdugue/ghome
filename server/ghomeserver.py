4c4a72b8121c7bfeca28532cf6f7a0d2866f8ed9
Ianic <ianicx@gmail.com>
2014-02-17 08:51:07 +0100

    homewatcher on ghomeserver

--- server/ghomeserver.py
+++ server/ghomeserver.py
@@ -87,6 +87,27 @@ class Database:
 			return result[0][0]
 			
 
+class HomeWatcher(object):
+	"""docstring for HomeWatcher"""
+	def __init__(self, arg):
+		super(HomeWatcher, self).__init__()
+
+	def getTime():
+		now = datetime.datetime.now()
+		return now.hour + now.minute + now.second()
+
+	def getWeekday():
+		now = datetime.datetime.now()
+		return now.weekday()
+
+	def getTemperature():
+		
+
+	def getWeatherCondition():
+		
+
+	def getPresence():
+
 
 def traiterTrame(trame):
 	tr = Trame(trame)
