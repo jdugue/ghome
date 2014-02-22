508414231ad7a9fa8f68a6481dc22a788930d09c
Vincent <vincent@ubuntu.(none)>
2014-02-22 18:35:14 +0100

    modif actionneur

--- webghome/hexanhome/actionneur_learning.py
+++ webghome/hexanhome/actionneur_learning.py
@@ -3,3 +3,19 @@ def getFictiveButtonId (id):
 	secondPart = str(id).zfill(4)
 	ID = firstPart+secondPart
 	return ID
+	
+def getTrameON (id):
+	firstPart = 'A55A6B05'
+	dataBytes = '70000000'
+	idBytes = id
+	endPart = '30FF'
+	completeTrame = firstPart+dataBytes+idBytes+endPart
+	return completeTrame
+	
+def getTrameOFF (id):
+	firstPart = 'A55A6B05'
+	dataBytes = '90000000'
+	idBytes = id
+	endPart = '30FF'
+	completeTrame = firstPart+dataBytes+idBytes+endPart
+	return completeTrame
