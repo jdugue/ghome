9872676e3850e78990311634a5fb5e0459b10910
Vincent <vincent@ubuntu.(none)>
2014-02-22 23:13:02 +0100

    HAHAHAAAAA

--- webghome/hexanhome/actionneur_learning.py
+++ webghome/hexanhome/actionneur_learning.py
@@ -1,3 +1,6 @@
+import socket
+
+# Return a fictive ID based on id parameter
 def getFictiveButtonId (id):
 	firstPart = 'FF9F'
 	secondPart = str(id).zfill(4)
@@ -19,3 +22,11 @@ def getTrameOFF (id):
 	endPart = '30FF'
 	completeTrame = firstPart+dataBytes+idBytes+endPart
 	return completeTrame
+	
+def sendTrameToServer (trameList):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server_adress = ('134.214.106.23', 5000)
+	sock.connect(self.server_adress)
+	for trame in trameList:
+		sock.send(trame)
+	sock.close
